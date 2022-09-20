308c574153b2ebca5ada6f4726b2c1d797a1530f
Shedrack Sunday <Hannania002@gmail.com>
2022-09-20 21:21:42 +0100

    add js file

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-const listBooks = document.querySelector('.books-list');
+const listBooks = document.querySelector('.list-books');
 const form = document.querySelector('.form-input');
 const [title, author] = form.elements;
 
-class Book {
-  constructor(title = "", author = "") {
-    this.title = title;
-    this.author = author;
-  }
-}
-
-class bookList {
-  constructor(books = []) {
-    this.books = books;
-  }
-
-  addBook(newBook) {
-    this.books.push(newBook);
-    localStorage.setItem('savedBooks', JSON.stringify(this.books));
-  }
-
-  removeBook(book) {
-    let result = this.books.filter((b) => b !== book);
-    this.books = result;
-    localStorage.setItem('savedBooks', JSON.stringify(this.books));
-  }
-}
-
-let books = new bookList();
+const inputBook = {};
+let books = new Array([]);
 
 if (localStorage.savedBooks) {
   books = JSON.parse(localStorage.getItem('savedBooks'));
 }
-
-const list = new bookList(books);
-const inputBook = new Book();
 
 title.addEventListener('change', () => {
   inputBook.title = title.value;
@@ -43,36 +17,58 @@ author.addEventListener('change', () => {
   inputBook.author = author.value;
 });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  list.addBook(new Book(inputBook.title, inputBook.author));
-  form.submit();
-  displayBooks();
-});
-
-const displayBooks = () => {
-  listBooks.innerHTML = '';
-  books.map((book) => {
-    const bookDiv = document.createElement('div');
-    const titleBook = document.createElement('p');
-    const authorBook = document.createElement('p');
-    titleBook.textContent = book.title;
-    authorBook.textContent = book.author;
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Remove';
-    const hrElementet = document.createElement('hr');
-    bookDiv.appendChild(titleBook);
-    bookDiv.appendChild(authorBook);
-    bookDiv.appendChild(deleteBtn);
-    bookDiv.appendChild(hrElementet);
-    listBooks.appendChild(bookDiv);
-
-    deleteBtn.addEventListener('click', () => {
-      list.removeBook(book);
-      listBooks.removeChild(bookDiv);
-    });
-    return listBooks;
-  });
+const populateFields = () => {
+  localStorage.setItem('savedBooks', JSON.stringify(books));
 };
 
-displayBooks();
+const Book = class {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  static removeBook(book) {
+    const result = books.filter((b) => b !== book);
+    books = result;
+    populateFields();
+  }
+
+  static addBook = (newBook) => {
+    books.push(newBook);
+    populateFields();
+    this.displayBooks();
+  };
+
+  static displayBooks = () => {
+    listBooks.innerHTML = '';
+    books.map((book) => {
+      const bookDiv = document.createElement('tr');
+      const elementBook = document.createElement('td');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Remove';
+
+      elementBook.textContent = `"${book.title}" by ${book.author}`;
+
+      bookDiv.classList.add('book-container');
+      bookDiv.appendChild(elementBook);
+      bookDiv.appendChild(deleteBtn);
+
+      listBooks.appendChild(bookDiv);
+
+      deleteBtn.addEventListener('click', () => {
+        this.removeBook(book);
+        listBooks.removeChild(bookDiv);
+      });
+      return listBooks;
+    });
+  };
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  Book.addBook(new Book(inputBook.title, inputBook.author));
+  form.submit();
+});
+
+Book.displayBooks();
+populateFields();
